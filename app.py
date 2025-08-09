29e82c448957fca22a9e251a3d0f064ac9ff98e8
Thiago Pelle <thiagopython007@gmail.com>
2025-08-08 22:28:22 -0400

    redmei atualizado

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 from openpyxl import load_workbook
 
-load_workbook("")
+planilha_vendas = load_workbook('vendas_carros.xlsx')
+pagina_vendas = planilha_vendas['Sheet1']
+
+for linha in pagina_vendas.iter_rows(values_only=True):
+    print(linha)
